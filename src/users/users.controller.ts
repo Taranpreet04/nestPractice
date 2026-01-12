ee29c7acc5075a9d978cfcea10dd84c6fa0383d8
Taranpreet04 <taranpreet@shinedezign.com>
2026-01-12 18:10:55 +0530

    first commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+     constructor(private usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() body) {
+    return this.usersService.create(body);
+  }
+
+  @Get()
+  getUsers() {
+      return this.usersService.findAll();
+    //   return[];
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+  async create(data: Partial<User>) {
+    const user = new this.userModel(data);
+    return user.save();
+  }
+
+  async findAll() {
+    return this.userModel.find().select('-password');
+  }
+}
